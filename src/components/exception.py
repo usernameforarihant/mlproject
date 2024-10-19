452cbbccc4b4f74250bf92c4b3aab631fae48277
usernameforarihant <usernameforarihant@gmail.com>
2024-10-19 20:58:54 +0530

    EDA and problem statement

--- src/components/exception.py
+++ src/components/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -7,7 +7,7 @@ def error_message_detail(error,error_detail:sys):
     error_message="Error occured in python script name[{0}] line number [{1}] error message[{2}]".format(file_name,exc_tb.tb_lineno,str(error))
     return error_message
 
-class CustomeException(Exception):
+class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
@@ -21,7 +21,7 @@ if __name__=="__main__":
         a=1/0
     except Exception as e:
         logging.info("Divide by zero")
-        raise CustomeException(e,sys)
+        raise CustomException(e,sys)
  
 
 
