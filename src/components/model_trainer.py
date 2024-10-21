d2bb098bf1affe2dfc3d33852d826d2e672d3813
usernameforarihant <usernameforarihant@gmail.com>
2024-10-21 09:14:17 +0530

    HyperParameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -38,8 +38,40 @@ class ModelTrainer:
                 'CatBoostRegressor': CatBoostRegressor(verbose=0),  # Suppress output of CatBoost
                 'KNeighborsRegressor': KNeighborsRegressor()
             }
+            params = {
+    "DecisionTreeRegressor": {
+        'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+    },
+    "RandomForestRegressor": {
+        'n_estimators': [8, 16, 32, 64, 128, 256],
+    },
+    "GradientBoostingRegressor": {
+        'learning_rate': [.1, .01, .05, .001],
+        'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+        'n_estimators': [8, 16, 32, 64, 128, 256],
+    },
+    "LinearRegression": {},
+    "XGBRegressor": {
+        'learning_rate': [.1, .01, .05, .001],
+        'n_estimators': [8, 16, 32, 64, 128, 256],
+    },
+    "CatBoostRegressor": {
+        'depth': [6, 8, 10],
+        'learning_rate': [0.01, 0.05, 0.1],
+        'iterations': [30, 50, 100],
+    },
+    "AdaBoostRegressor": {
+        'learning_rate': [.1, .01, 0.5, .001],
+        'n_estimators': [8, 16, 32, 64, 128, 256],
+    },
+    "KNeighborsRegressor": {}
+}
 
-            model_report: dict = evaluate_models(X_train=X_train,X_test=X_test, y_train=y_train, y_test=y_test, models=models)
+                
+            
+
+            # model_report: dict = evaluate_models(X_train=X_train,X_test=X_test, y_train=y_train, y_test=y_test, models=models)
+            model_report: dict = evaluate_models(X_train=X_train,X_test=X_test, y_train=y_train, y_test=y_test, models=models,params=params)
             best_model_score = max(list(sorted(model_report.values())))
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
             best_model = models[best_model_name]
@@ -58,4 +90,4 @@ class ModelTrainer:
             return r2_sc
 
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import numpy as np
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_obj(file_path,obj):
     try:
@@ -16,33 +17,54 @@ def save_obj(file_path,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
-# def evaluate_models(X_train,y_train,X_test,y_test,models):
+# def evaluate_models(X_train,y_train,X_test,y_test,models,params):
 #     try:
 #         report={}
 #         for i in range(len(list(models))):
 #             model=list(models.values())[i]
+#             para=params[list(models.keys())[i]]
+
+#             gs=GridSearchCV(model,para,cv=3)
+#             gs.fit(X_train,y_train)
+
+#             model.set_params(**gs.best_params_)
 #             model.fit(X_train,y_train)
+
 #             y_train_pred=model.predict(X_train)
 #             y_test_pred=model.predict(X_test)
+
 #             train_model_score=r2_score(y_train,y_train_pred)
 #             test_model_score=r2_score(y_test,y_test_pred)
-#             report[list(models.keys())[i]] = test_model_score
-#             return report
+
+#             report[list(model.keys())[i]]=test_model_score
+
+#         return report
+    
 #     except Exception as e:
-#         raise CustomException(e,sys)
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+#         raise CustomException 
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         for i in range(len(list(models))):
-            model = list(models.values())[i]
             model_name = list(models.keys())[i]
+            model = list(models.values())[i]
+            para = params[model_name]  # Ensure the key matches here
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
+
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
+
             report[model_name] = test_model_score
+
         return report
+
     except Exception as e:
         raise CustomException(e, sys)
-
